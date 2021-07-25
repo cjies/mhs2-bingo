4a6dcc56f947c2512101991d83d3ccdc660e7361
CJ Tan <cijies@gmail.com>
2021-07-25 23:27:39 +0800

    fix: reduce gene size on mobile

--- components/Bingo/constants.ts
+++ components/Bingo/constants.ts
@@ -1,5 +1,5 @@
 export const GENE_SIZE = 8; // in rem
-export const GENE_SIZE_XS = 6; // in rem
+export const GENE_SIZE_XS = 5.5; // in rem
 
 export const GENE_GAP = 2; // in rem
 export const GENE_GAP_XS = 1.5; // in rem
