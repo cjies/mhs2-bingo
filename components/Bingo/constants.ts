4c6ef15ee85d16c489b6fa2f4b0a5cd2e22f3ad9
CJ Tan <xijie.tan@grabtaxi.com>
2021-07-26 09:50:57 +0800

    fix: smaller line size

--- components/Bingo/constants.ts
+++ components/Bingo/constants.ts
@@ -4,5 +4,5 @@ export const GENE_SIZE_XS = 5.5; // in rem
 export const GENE_GAP = 2; // in rem
 export const GENE_GAP_XS = 1.5; // in rem
 
-export const GENE_LINE_SIZE = 1; // in rem
-export const GENE_LINE_SIZE_XS = 0.8; // in rem
+export const GENE_LINE_SIZE = 0.8; // in rem
+export const GENE_LINE_SIZE_XS = 0.6; // in rem
